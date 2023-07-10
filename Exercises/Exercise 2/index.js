bcdde27477f9d5b7aa64a74ad152b763ba364060
Engy Ibrahim <81655040+Engyyyy@users.noreply.github.com>
2023-07-10 15:52:18 +0300

    coding ex 2

---
+++ Exercises/Exercise 2/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
+function App() {
+  return (
+    <div className="wrapper">
+      <textarea
+        readOnly
+        maxLength={3}
+        spellCheck
+        style={{backgroundColor: 'gray'}}
+      />
+    </div>
+  );   
+}
+
+root.render(<App />);
