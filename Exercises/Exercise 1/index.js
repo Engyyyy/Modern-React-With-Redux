61c1a318fbe24dd0040019735b75f1ebbb78b2b4
Engy Ibrahim <81655040+Engyyyy@users.noreply.github.com>
2023-07-10 14:58:04 +0300

    coding ex1

---
+++ Exercises/Exercise 1/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
+function App() {
+  const name = 'Engy';
+
+  return (
+    <div>
+      My name is:
+      <h1>{name}</h1>
+    </div>
+  );
+}
+
+root.render(<App />);
