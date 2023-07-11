0205e889a92229d6b07549182787aae8a69b17d4
Engy Ibrahim <81655040+Engyyyy@users.noreply.github.com>
2023-07-11 10:13:03 +0300

    coding ex 4

---
+++ Exercises/Exercise 4/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    const handleClick = () => console.log("Hello there!");
+    return (
+        <div>
+            <button onClick={handleClick} >Click Me!</button>          
+        </div>
+    );
+}
+
+export default App;
